06128c65cf019587a334a175f3fe10c5c7b553e0
hato-taka <n28hashimoto@gmail.com>
2024-07-21 19:52:07 +0900

    ADD: 表の追加

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 st.title('麻雀部')
 st.write('東中野麻雀部の結果発表')
+
+data = {
+    'name': ['Naoki', 'Ohashi', 'ゆたか'],
+    'score': [100, 200,300]
+}
+
+df = pd.DataFrame(data)
+
+st.dataframe(df)
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    st.write(uploaded_file)
