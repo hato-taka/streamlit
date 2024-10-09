04e468fd4dbc7d2e763b2d35149657fd99ef6cca
hato-taka <n28hashimoto@gmail.com>
2024-10-09 19:13:04 +0900

    PRE-RELEASE

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # streamlit
+アプリの起動
+`streamlit run app.py`
+
+テキスト表示
+```python
+st.title("title") # タイトル
+st.header("header") # ヘッダー
+st.write("write") # 表示
+st.markdown("# markdown") # マークダウンで表示
+st.text("text") # テキスト表示
+```
+
+参照元: https://www.alpha.co.jp/blog/202304_02/
--- app.py
+++ app.py
@@ -1,30 +1,26 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import matplotlib
+import matplotlib.dates as mdates
 
-st.image("top.jpg", caption="麻雀の写真")
-st.title('麻雀部')
+
+st.title('東中野 Mリーグ')
+st.image("top.jpg")
 st.write('東中野麻雀部の結果発表')
 
-print(np.random.randn(20, 3))
-two_dimensional_list = [
-    [1, 9, 3],
-    [4, 7, 6],
-    [7, 3, 9]
-]
-chart_data = pd.DataFrame(two_dimensional_list, columns=["a", "b", "c"])
-
-st.line_chart(chart_data, x_label="日にち", y_label="得点")
+st.header("順位表 (10月7日更新)")
 
 data = {
-    'name': ['Naoki', 'Ohashi', 'ゆたか'],
-    'score': [100, 200,300]
+    '雀士名': ['コペ', 'せいか', 'ゆたか', 'しゅん', 'ぐっさん', 'あーちゃん', 'おーはし', 'なおき', 'こじ'],
+    '平均順位': [2.29, 2.48, 2.39, 2.53, 2.53, 2.45, 2.49, 2.81, 3.25],
+    '平均得点': [6.94, 4.24, 2.71, -0.01, -0.71, -0.77, -1.40, -9.39, -30.33]
 }
 
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, index=['1位','2位','3位', '4位', '5位', '6位', '7位', '8位', '-'])
 
 st.dataframe(df)
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    st.write(uploaded_file)
+st.image("graph.jpg")
