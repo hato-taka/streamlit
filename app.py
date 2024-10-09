6c76abd9fc847c800f6faffc8b2c5e3ad3129edd
hato-taka <n28hashimoto@gmail.com>
2024-10-09 19:16:51 +0900

    FIX

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import japanize_matplotlib
-import matplotlib
-import matplotlib.dates as mdates
 
 
 st.title('東中野 Mリーグ')
